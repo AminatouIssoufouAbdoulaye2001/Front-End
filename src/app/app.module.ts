29c19812d6985799191b6a4986ca43f8317edb91
Oummyia <aminatouoummyia@gmail.com>
2022-04-18 10:02:28 +0100

    probleme mdb resolut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,21 +28,7 @@ import { DashboardComponent } from './ClientDashBoard/dashboard/dashboard.compon
 import { TopComponent } from './ClientDashBoard/top/top.component';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
 import { DomaineComponent } from './Components/domaine/domaine.component';
 
 @NgModule({
@@ -78,22 +64,7 @@ import { DomaineComponent } from './Components/domaine/domaine.component';
     MatToolbarModule,
     BrowserAnimationsModule,
     CardModule,
-    ButtonModule,
-    MdbAccordionModule,
-    MdbCarouselModule,
-    MdbCheckboxModule,
-    MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
+    ButtonModule, 
   ],
   providers: [],
   bootstrap: [AppComponent],
