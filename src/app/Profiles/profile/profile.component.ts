5430a30efba8a58e06d84bb6b30545fdd70ab1c1
abdellah098 <saley.abdellah@gmail.com>
2022-05-06 13:17:55 +0200

    correction du design dashboard client

--- src/app/Profiles/profile/profile.component.ts
+++ src/app/Profiles/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-profile',
@@ -7,15 +8,40 @@ import {FormBuilder} from "@angular/forms";
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit, OnDestroy {
+  profilForm: FormGroup;
+  changePassworForm: FormGroup;
+  sub = new Subscription();
 
   constructor(private fb: FormBuilder) {
+    this.changePassworForm = fb.group({
+      oldPassword: fb.control('', Validators.required),
+      newPassword: fb.control('', Validators.required),
+      ConfNewPassword: fb.control('', Validators.required),
+    })
 
+    this.profilForm = fb.group({
+      username: fb.control('', Validators.required),
+      fullName: fb.control('', Validators.required),
+      email: fb.control('', [Validators.required, Validators.email]),
+      phone: fb.control('', [Validators.required])
+    })
   }
 
   ngOnInit(): void {
 
   }
 
+  submitProfil() {
+
+  }
+
+  resetProfil() {
+
+  }
+
+  submitChangePassword() {
+
+  }
   ngOnDestroy(): void {
 
   }
